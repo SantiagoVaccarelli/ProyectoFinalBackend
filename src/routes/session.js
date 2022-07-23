d8eb1eb31fe0f690f3670734f1ac53327d0ca44c
Santiago Vaccarelli <svaccarelli@fi.uba.ar>
2022-07-23 14:05:52 -0300

    Added passportjs

--- src/routes/session.js
+++ src/routes/session.js
@@ -1,22 +1,3 @@
-// const { Router } = require("express");
-
-// const router = Router();
-
-// router.get("/", (req, res) => {
-//     res.render("session");
-// });
-
-// router.post("/session", (req, res) => {
-//     console.log(req.body);
-//     for(let key in req.body) {
-//         if(req.body[key]) { req.session[key] = req.body[key]; }
-//     }
-//     let name = req.body.name
-//     res.render('bienvenida/index.ejs', {name});
-// });
-
-// module.exports = router
-
 import { Router } from "express";
 
 const router = Router();
